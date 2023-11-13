aea7015150a58e07626769f70ca28e52692e4012
Akshay Gaddi <76643881+organicboi@users.noreply.github.com>
2023-11-13 14:02:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const RegisterModel = require("./models/Register");
 const app = express();
 app.use(
   cors({
-    origin: ["https://new-youtbe-try-frontend.vercel.app/"],
+    origin: ["https://new-youtbe-try-frontend.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
   })
