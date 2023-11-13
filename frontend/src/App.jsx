a18183e1b7a24883c698a78fa64007a044ec6b9b
Akshay Gaddi <76643881+organicboi@users.noreply.github.com>
2023-11-13 14:31:55 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     axios
-      .post("https://new-youtube-try-api.vercel.app/register", {
+      .post("https://new-youtbe-try.vercel.app//register", {
         name,
         email,
         password,
